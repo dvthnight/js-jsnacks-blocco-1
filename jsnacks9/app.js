3b736f9ad8072508004982f1dc9dcf5ad5fd60b6
dvthnight <davide.lanotte@protonmail.com>
2022-02-11 18:49:57 +0100

    completato jsnacks9

--- jsnacks9/app.js
+++ jsnacks9/app.js
@@ -1,1 +1,12 @@
 /* Calcola la somma e la media dei primi 10 numeri. */
+
+let somma=0;
+
+for(let i =1; i<=10; i++){
+    somma+=i;
+}
+
+const media = somma / 10;
+
+console.log("la somma è "+ somma);
+console.log("la media è "+media);
