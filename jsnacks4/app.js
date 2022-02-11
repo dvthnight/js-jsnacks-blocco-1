f265bdc6247579b15683ee4c4330eb5e1e85e128
dvthnight <davide.lanotte@protonmail.com>
2022-02-11 17:28:06 +0100

    completato jsnacks4

--- jsnacks4/app.js
+++ jsnacks4/app.js
@@ -1,2 +1,21 @@
 /* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. */
+
+const invitati = ["Giovanni","Antonio","Pino","Francesco","Pierpaolo","Tommaso"];
+
+const nomeUtente = prompt("inserisci il tuo nome");
+
+let j = 0;
+
+for(let i=0; i< invitati.length; i++){
+    if(nomeUtente===invitati[i]){
+        console.log("Sei sulla lista degli invitati. Puoi entrare.");
+        continue;
+    } else{
+        j++;
+    }
+}
+
+if(j===invitati.length){
+    console.log("Non sei nella lista degli invitati. Non puoi entrare.")
+}
