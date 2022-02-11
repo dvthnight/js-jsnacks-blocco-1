db2363d0aa1758cc83b8b8092519f2b17d54f6a6
dvthnight <davide.lanotte@protonmail.com>
2022-02-11 17:35:22 +0100

    completato jsnacks5

--- jsnacks5/app.js
+++ jsnacks5/app.js
@@ -1,2 +1,15 @@
 /* Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, 
 se è dispari inseriscilo nell’array. */
+
+const array = [];
+let j= 0;
+
+for(let i = 0; i<6; i++){
+    const numero = parseInt(prompt("inserisci un numero"));
+    if(!isNaN(numero) && numero%2 != 0){
+        array[j]= numero;
+        j++;
+    }
+}
+
+console.log(array);
