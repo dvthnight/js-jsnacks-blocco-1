7bc09dae6f3b009125c4700674f0953da4cfe286
dvthnight <davide.lanotte@protonmail.com>
2022-02-11 17:43:02 +0100

    completato jsnacks6

--- jsnacks6/app.js
+++ jsnacks6/app.js
@@ -1,1 +1,9 @@
 /* Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente. */
+
+const numeroUtente = parseInt(prompt("inserisci un numero"));
+
+
+for(let i=1; i<=numeroUtente; i++){
+    const cubo = Math.pow(i, 3);
+    console.log(cubo);
+}
